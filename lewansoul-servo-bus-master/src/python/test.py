ad4735bd78cc8266e44d83b1268560b9466f0355
Can Guven <cguven@wpi.edu>
2023-10-10 17:47:56 -0400

    servo library and test

---
+++ lewansoul-servo-bus-master/src/python/test.py
@@ -0,0 +1,15 @@
+from lewansoul_servo_bus import ServoBus
+
+servo_bus = ServoBus('/dev/cu.usbserial-0001')
+
+# Move servo with ID 1 to 90 degrees in 1.0 seconds
+#servo_bus.move_time_write(1, 90, 1.0)
+
+servo_1 = servo_bus.get_servo(1)
+servo_1.move_time_write(180, 2)
+servo_1.velocity_read(1,2)
+
+# Move servo with ID 2 to 180 degrees in 2.0 seconds
+servo_2 = servo_bus.get_servo(2)
+servo_2.move_time_write(180, 2)
+
