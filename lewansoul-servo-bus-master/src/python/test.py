df454d1428f8a2b8bba85ce280cc955d4326c0d1
caanguven <30950184+caanguven@users.noreply.github.com>
2023-10-11 14:54:02 -0400

    Update test.py

--- lewansoul-servo-bus-master/src/python/test.py
+++ lewansoul-servo-bus-master/src/python/test.py
@@ -4,8 +4,8 @@ servo_bus = ServoBus('/dev/ttyUSB0')
 
 #servo_bus.move_time_write(1, 90, 1.0)
 servo_1 = servo_bus.get_servo(1)
-servo_1.move_time_write(180, 2)
+servo_1.move_time_write(45, 2)
 
 # Move servo with ID 2 to 180 degrees in 2.0 seconds
 servo_2 = servo_bus.get_servo(2)
-servo_2.move_time_write(180, 2)
+servo_2.move_time_write(45, 2)
